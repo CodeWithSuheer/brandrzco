f95a2b3fd78cba086752873493459991b1037082
Suheer Zahid <glowingemp124@glowingsoft.com>
2025-08-11 10:10:30 +0500

    Update loader

--- src/components/LoaderWrapper.tsx
+++ src/components/LoaderWrapper.tsx
@@ -1,9 +1,12 @@
-
 "use client";
 import React, { useState, useEffect } from "react";
 import "./Components.css";
 
-export default function LoaderWrapper({ children }: { children: React.ReactNode }) {
+export default function LoaderWrapper({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const [loading, setLoading] = useState(true);
   useEffect(() => {
     const timer = setTimeout(() => setLoading(false), 1500);
@@ -13,8 +16,12 @@ export default function LoaderWrapper({ children }: { children: React.ReactNode 
   return loading ? (
     <div className="loader-overlay">
       <div className="loader-center">
-        <div className="loader" />
-        <h2 className="loader-title">Loading BrandrsCo...</h2>
+        <img
+          src="/brandrzCo1.gif"
+          alt="Loading..."
+          className="loader-gif h-24 w-24"
+        />
+        <h2 className="loader-title">Loading...</h2>
       </div>
     </div>
   ) : (
